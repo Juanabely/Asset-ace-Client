404f0899d071bd3735e342ba9083e275fbe6b5fc
Juanabely <abelkipkorir185@gmail.com>
2024-05-08 19:16:45 +0300

    Home css adjustment

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,16 +1,12 @@
 import React from 'react'
 import './home.css'
-import Frame3 from '../../assets/Frame1.svg'
-import Frame2 from '../../assets/Frame2.svg'
-import Frame1 from '../../assets/Frame3.svg'
-import Frame4 from '../../assets/Frame4.svg'
 import { RadarChartOutlined } from '@ant-design/icons'
 
 
 function Home() {
   return (
     <section className="h-wraper">
-        <div className="flexCenter h-wrap">
+        <div className="paddings innerWidth flexCenter h-wrap">
             <div className="flexColStart h-left">
                 <span className="primaryText">
                 Unlock Your Potential <br />
@@ -19,17 +15,12 @@ function Home() {
                 </span>
                 <button className='button btn'>Get started</button>
             </div>
-            <div className=" h-right">
-                <div id="frame6"><img src="./Frame6.svg" alt="" /></div>
-                <div id="img"><img src="./Frame5-rep.svg" alt="" /></div>
-                <div id="img2"><img src="./image9.svg" alt="" /></div>
-                <div id="dots"><img src="./Group.svg" alt="" /></div>
-
-                {/* <div id='circle1'><img src={Frame1} alt="" width={196}/></div>
-                <RadarChartOutlined size={100}/>
-                <div id='circle2'><img src={Frame2} alt="" width={196}/></div>
-                <div id='circle3'><img src={Frame3} alt="" width={196}/></div>
-                <div id="box"><img src={Frame4} alt="" /></div> */}
+            <div className="flexCenter h-right">
+             <div className="image-container">
+                <img src="./image9.svg" alt="" />
+                <img src="./Group.svg" alt="" />
+                <img src="./Group.svg" alt="" />
+             </div>
             </div>
         </div>
     </section>
